172ad501c2f2e9375dcb1114b2d372427d5bf7d9
talentdatigan <u07dd18@abdn.ac.uk>
2019-11-19 16:50:50 +0000

    Fixed order page and updated gemfile for heroku

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  
   # GET /orders
   # GET /orders.json
   def index
@@ -26,12 +27,7 @@ class OrdersController < ApplicationController
     @credit_cards = CreditCard.all
   end
 
-  # GET /orders/1/edit
-  def edit
-    @pokedexes = Pokedex.all
-    @credit_cards = CreditCard.all
-    @addresses = Address.all
-  end
+
 
   # POST /orders
   # POST /orders.json
@@ -53,29 +49,8 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -85,6 +60,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:price, :street, :city, :postcode, :card_no, :exp_date, :name_on_card, :organisation, :address_id, :credit_card_id, :user_id, :pokedex_id)
+      params.require(:order).permit(:price, :street, :city, :postcode, :card_no, :exp_date, :name_on_card, :organisation,  :pokedex_id,:address_id,:credit_card_id)
     end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   belongs_to :credit_card
   belongs_to :user
   belongs_to :pokedex
+  validates  :street, :city, :postcode, :card_no, :exp_date, :name_on_card, :organisation, :user_id, :pokedex_id, presence: true
 end
